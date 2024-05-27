5f2bf9683459fc890d2dd793f05e4b5ed614c5c5
tarasmaliarchuk <tarasmalarcuk8@gmail.com>
2024-05-28 02:55:13 +0300

    chores

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {join} = require("path");
 const getWeather = require("./util/getWeather")
 const getServerIP = require("./util/getIp")
 const app = express()
-const axios = require('axios');
+const axios = require('axios')
 const PORT = 3000
 const ipgeolocationApiKey = "ac5a71156db34075a7b3a330842dc440"
 
@@ -25,17 +25,17 @@ app.get('/weather', async (req, res) => {
         if (clientIp === '::1' || clientIp === '127.0.0.1') {
             clientIp = await getServerIP()
         }
-        const response = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${ipgeolocationApiKey}&ip=${clientIp}&lang=en&fields=city`);
+        const response = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${ipgeolocationApiKey}&ip=${clientIp}&lang=en&fields=city`)
         const city = response.data.city
         await getWeather(city, res)
     } catch (error) {
-        console.error('Error fetching geolocation data:', error);
-        res.status(500).json({ error: 'Error fetching geolocation data' });
+        console.error('Error fetching geolocation data:', error)
+        res.status(500).json({ error: 'Error fetching geolocation data' })
     }
 });
 
 app.get('/weather/:city', async (req, res) => {
-    const city = req.params.city;
+    const city = req.params.city
     await getWeather(city, res)
 });
 
